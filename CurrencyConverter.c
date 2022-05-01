80a88769481565e1030174a0a46bc1322aaad7e5
sherifKhamis <sherifookhamis@gmail.com>
2022-05-01 13:55:42 +0200

    Added comments

--- CurrencyConverter.c
+++ CurrencyConverter.c
@@ -1,24 +1,29 @@
+/*Very simple Currency Converter in C with 
+ 4 currencies: EURO, US Dollar, British Pound and Chinese Yen*/ 
+
 #include <stdio.h>
-#include <string.h>
+#include <string.h> //for strypy()
 
 int main ()
 {
+    //Header and starting currency menu
     printf("          Currency Converter          \n\n");
     printf("Which currency do you want to exchange ?\n");
     printf("     (a) EUR     (b) USD     (c) GBP     (d) CNY \n");
-    char input, input2;
-    char icurrency [3];
-    char icurrency2 [3];
-    char ocurrency [3];
-    char ocurrency2 [3];
+    char input, input2, icurrency [3], ocurrency [3], output;
     scanf(" %c", &input);
+
+    //While loop to limit user input to "abcd" only for starting currency input
     while (input != 'a' && input != 'b' && input != 'c' && input != 'd')
     {
         printf("\n\nWrong input, try again !\n\n");
         printf("     (a) EUR     (b) USD     (c) GBP     (d) CNY \n");
         scanf(" %c", &input);
     }
-    input2 = input;
+    input2 = input; //Since "input" gets unassigned after being used in the following switch statement
+                    //the value of "input" has to be saved in "input2" for later use
+
+    /* Switch statement to assign a real currency name to the single letter user input*/
     switch (input)
     {
         case 'a':
@@ -37,14 +42,17 @@ int main ()
         strcpy(icurrency, "CNY");
         break;
     }
+    
+    //Menu for starting currency amount and target currency 
     printf("\n\nHow much %s do you have ? ", icurrency);
-    float amount;
+    float amount, multiplyer;
     scanf("%f", &amount);
     printf("\n\nWhich currency do you want instead ?\n");
     printf("     (a) EUR     (b) USD     (c) GBP     (d) CNY \n");
-    char output;
     scanf(" %c", &output);
-    float multiplyer;
+
+    /* Switch statement to assign a real currency name to the single letter user input and set
+       "multiplyer" to the needed exchange rate*/
     switch (output)
     {
         case 'a':
@@ -123,7 +131,9 @@ int main ()
         }
         break;
     }
-    float result = amount * multiplyer;
+    float result = amount * multiplyer; //result = starting currency amount * exchange rate
+
+    //Output result 
     printf("\n\nYour %.2f %s are %.2f %s ", amount, icurrency, result, ocurrency);
 
     return 0;
