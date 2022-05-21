fc6bac1d4128a907cc5157bf171ab5e40afb2829
sherifKhamis <sherifookhamis@gmail.com>
2022-05-21 16:11:06 +0200

    Comments left

---
+++ TicTacToe.cpp
@@ -0,0 +1,171 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void printGame (vector <vector <char>> field);
+bool validPlay (int row, int column, vector <vector <char>> field);
+void playGame (vector <vector <char>> field);
+bool Owin (vector <vector<char>> board);
+bool Xwin (vector <vector<char>> board);
+
+int main ()
+{
+    vector <vector <char>> defaultField = 
+    {
+        {'-', '-', '-'},
+        {'-', '-', '-'},
+        {'-', '-', '-'},
+    };
+
+    playGame(defaultField);
+
+
+}
+
+void printGame (vector <vector <char>> field)
+{
+    for (int i=0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << " | ";
+            cout << field[i][j];
+        }
+        cout << " | ";
+        cout << endl;
+        cout << "---------------" << endl;
+    }
+}
+
+void playGame (vector <vector <char>> field)
+{
+    cout << "Welcome to TicTacToe in C++"<<endl;
+    printGame(field);
+    cout << "\nPlayer 1: Which ROW do you want to play ?:  ";
+    int row, column;
+    cin >> row;
+    cout << "\nPlayer 1: Which COLUMN do you want to play ?:  ";
+    cin >> column;
+    while (!validPlay(row, column, field))
+    {
+        cout << "\nField already completed choose another one !\n";
+        printGame(field);
+        cout << "\nPlayer 1: Which row do you want to choose ?: ";
+        cin >> row;
+        cout << "\nPlayer 1: Which column do you want to choose ?: ";
+        cin >> column;
+    }
+    field[row-1][column-1] = 'X';
+    printGame(field);
+    if (Xwin(field))
+    {
+        cout << "\nPlayer 1 won!";
+        return;
+    }
+
+    cout << "\nPlayer 2: Which ROW do you want to play ?:  ";
+    int row2, column2;
+    cin >> row2;
+    cout << "\nPlayer 2: Which COLUMN do you want to play ?:  ";
+    cin >> column2;
+    while (!validPlay(row2, column2, field))
+    {
+        cout << "\nField already completed choose another one !\n";
+        printGame(field);
+        cout << "\nPlayer 1: Which row do you want to choose ?: ";
+        cin >> row2;
+        cout << "\nPlayer 1: Which column do you want to choose ?: ";
+        cin >> column2;
+    }
+    field[row2-1][column2-1] = 'O';
+    printGame(field);
+    if (Xwin(field))
+    {
+        cout << "\nPlayer 2 won!";
+        return;
+    }
+    playGame(field);
+
+}
+
+bool validPlay (int row, int column, vector <vector <char>> field)
+{
+    if (field[row-1][column-1] != '-')
+        return false;
+    return true;
+}
+
+bool Xwin (vector <vector<char>> board)
+{
+       if (board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X')
+       {
+           return true;
+       }
+       if (board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X')
+       {
+           return true;
+       }
+       if (board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X')
+       {
+           return true;
+       }
+       if (board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X')
+       {
+           return true;
+       }       
+       if (board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X')
+       {
+           return true;
+       }       
+       if (board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X')
+       {
+           return true;
+       }
+       if (board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X')
+       {
+           return true;
+       }
+       if (board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X')
+       {
+           return true;
+       }
+       return false;
+}
+
+bool Owin (vector <vector<char>> board)
+{
+       if (board[0][0] == 'O' && board[0][1] == 'O' && board[0][2] == 'O')
+       {
+           return true;
+       }
+       if (board[1][0] == 'O' && board[1][1] == 'O' && board[1][2] == 'O')
+       {
+           return true;
+       }
+       if (board[2][0] == 'O' && board[2][1] == 'O' && board[2][2] == 'O')
+       {
+           return true;
+       }
+       if (board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'O')
+       {
+           return true;
+       }       
+       if (board[0][1] == 'O' && board[1][1] == 'O' && board[2][1] == 'O')
+       {
+           return true;
+       }       
+       if (board[0][2] == 'O' && board[1][2] == 'O' && board[2][2] == 'O')
+       {
+           return true;
+       }
+       if (board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O')
+       {
+           return true;
+       }
+       if (board[0][2] == 'O' && board[1][1] == 'O' && board[2][0] == 'O')
+       {
+           return true;
+       }
+       return false;
+}
