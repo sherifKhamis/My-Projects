a17f8579387629126e868c5624a684216a40dffc
Sherif <sherifookhamis@gmail.com>
2022-05-30 19:28:14 +0200

    Added printBoard()

---
+++ TicTacToe.py
@@ -0,0 +1,9 @@
+def printBoard(board):
+    print(" {} | {} | {}".format(board[0], board[1], board[2]))
+    print("-----------")
+    print(" {} | {} | {}".format(board[3], board[4], board[5]))
+    print("-----------")
+    print(" {} | {} | {}".format(board[6], board[7], board[8]))
+
+emptyBoard = ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
+
